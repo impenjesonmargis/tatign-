4099a0834ac98becc0a4652935e7f85329ee07bc
আলি হাসান <101947194+dev-alihasan@users.noreply.github.com>
2023-02-20 23:35:59 +0600

    Update solvedwithjavascript.js

--- solvedwithjavascript.js
+++ solvedwithjavascript.js
@@ -1,4 +1,3 @@
- //implementation of the function in js
 function longestCommonPrefix(strs) {
   if (strs.length === 0) {
     return "";
@@ -7,7 +6,7 @@ function longestCommonPrefix(strs) {
   let prefix = strs[0];
 
   for (let i = 1; i < strs.length; i++) {
-    while (strs[i] indexOf(prefix) !== 0) {
+    while (strs[i].indexOf(prefix) !== 0) {
       prefix = prefix.substring(0, prefix.length - 1);
       if (prefix === "") {
         return "";
@@ -17,4 +16,3 @@ function longestCommonPrefix(strs) {
 
   return prefix;
 }
-
